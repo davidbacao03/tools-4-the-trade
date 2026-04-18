f931503fc224ff5bdc4f3fa51a19bdf2600f078f
davidbacao03 <garrafaagua714@gmail.com>
2026-04-18 01:07:52 +0100

    cssloginregistar

--- frontend/pages/login.php
+++ frontend/pages/login.php
@@ -7,6 +7,7 @@
 <head>
 <title>Login - Tools 4 The Trade</title>
 <meta charset="utf-8">
+<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <h2>Login</h2>
--- frontend/pages/registar.php
+++ frontend/pages/registar.php
@@ -6,6 +6,7 @@
 <head>
 <title>Registar - Tools 4 The Trade</title>
 <meta charset="utf-8">
+<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <h2>Registar</h2>
